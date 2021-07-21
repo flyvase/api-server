b312b7bcb56677ca65fa0621363eda48c45ca087
Shunei <info@shuneihayakawa.biz>
2021-07-22 07:42:57 +0900

    added sql statement to create users table

---
+++ scripts/db/2_create_table.sql
@@ -0,0 +1,11 @@
+-- Only for local development env
+
+create table users (
+  id int unsigned primary key auto_increment,
+  firebase_uid varchar(255) not null unique,
+  first_name varchar(100) not null,
+  last_name varchar(100) not null,
+  created_at datetime not null default current_timestamp,
+  updated_at datetime not null default current_timestamp on update current_timestamp,
+  deleted_at datetime
+);
