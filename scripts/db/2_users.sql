1896f583eb6f9568fd5626ea20f8a2bd53356152
Shunei <info@shuneihayakawa.biz>
2021-07-22 07:58:52 +0900

    added insert statement example for users table

--- scripts/db/2_users.sql
+++ scripts/db/2_users.sql
@@ -1,5 +1,4 @@
 -- Only for local development env
-
 create table users (
   id int unsigned primary key auto_increment,
   firebase_uid varchar(255) not null unique,
@@ -9,3 +8,11 @@ create table users (
   updated_at datetime not null default current_timestamp on update current_timestamp,
   deleted_at datetime
 );
+
+-- insert statement example
+-- insert into users (firebase_uid, first_name, last_name)
+-- values (
+--     "cUOv9IiMsAgpzTzoUjkTpwW9Ilr2",
+--     "Shunei",
+--     "Hayakawa"
+--   );
