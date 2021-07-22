18e4a5cc63ed116fdf2b3da6a8c29011940683ca
Shunei <info@shuneihayakawa.biz>
2021-07-22 19:44:14 +0900

    added validations for path and http methods

--- handlers/helpers.go
+++ handlers/helpers.go
@@ -2,15 +2,32 @@ package handlers
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
 
 	"harvest/config"
 )
 
-func GetTraceId(header string) string {
-	parts := strings.Split(header, "/")
+func getTraceId(r *http.Request) string {
+	parts := strings.Split(r.Header.Get("X-Cloud-Trace-Context"), "/")
 	if len(parts) > 0 && len(parts[0]) > 0 {
 		return fmt.Sprintf("projects/%s/traces/%s", config.ProjectId, parts[0])
 	}
 	return ""
+}
+
+func validatePath(p string, w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != p {
+		http.NotFound(w, r)
+	}
+}
+
+func validateMethods(methods []string, w http.ResponseWriter, r *http.Request) {
+	for _, m := range methods {
+		if r.Method == m {
+			return
+		}
+	}
+
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 }
