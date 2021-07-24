6e55e804f86d3136fc56884583a0e4fcb8076c5a
Shunei <info@shuneihayakawa.biz>
2021-07-24 09:32:11 +0900

    added request body decoding on users handler

--- handlers/users.go
+++ handlers/users.go
@@ -2,11 +2,14 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
 
+	"github.com/pkg/errors"
 	"github.com/rs/cors"
 
 	"harvest/config"
+	"harvest/entities"
 	"harvest/logger"
 )
 
@@ -19,7 +22,14 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 	validateMethods([]string{http.MethodPost}, w, r)
 	validateContentType(jsonContentType, w, r)
 
-	logger.Info(ctx, "good", component)
+	var b entities.User
+	dec := json.NewDecoder(r.Body)
+	if err := dec.Decode(&b); err != nil {
+		logger.Error(ctx, component, errors.WithStack(err))
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
 }
 
 func UsersHandler() http.Handler {
