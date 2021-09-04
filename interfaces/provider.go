91941c4a10b2bb08a7e78f640f6f2b3f1ba513e2
Shunei <info@shuneihayakawa.biz>
2021-09-04 10:46:49 +0900

    remove whole previous code

--- interfaces/provider.go
+++
@@ -1,5 +0,0 @@
-package interfaces
-
-type RepositoriesProvider struct {
-	User User
-}
--- interfaces/users.go
+++
@@ -1,7 +0,0 @@
-package interfaces
-
-import "harvest/entities"
-
-type User interface {
-	Create(entities.User) error
-}
