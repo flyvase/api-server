1edc04d13e004b0e05c89a74b5fa3d10c2d9286a
Shunei <info@shuneihayakawa.biz>
2021-09-04 21:24:55 +0900

    added firebase admin sdk setup

--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module harvest
 go 1.15
 
 require (
+	firebase.google.com/go/v4 v4.6.0
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/rs/cors v1.8.0
 	github.com/stretchr/testify v1.6.1 // indirect
+	google.golang.org/api v0.40.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
 
+	firebase "firebase.google.com/go/v4"
 	"github.com/rs/cors"
+	"google.golang.org/api/option"
 
 	"harvest/config"
 	"harvest/core/logger"
@@ -13,9 +16,32 @@ import (
 	"harvest/infrastructure/web/handler"
 )
 
+func initializeFirebase() (*firebase.App, context.Context) {
+	ctx := context.Background()
+
+	if config.Mode == "debug" && config.Environment == "dev" {
+		opt := option.WithCredentialsFile("keys/harvest-firebase-admin-sa-key.json")
+		app, err := firebase.NewApp(ctx, nil, opt)
+		if err != nil {
+			panic(err)
+		}
+
+		return app, ctx
+	} else {
+		app, err := firebase.NewApp(ctx, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		return app, ctx
+	}
+}
+
 func main() {
 	log.SetFlags(0)
 	logger.Debug("Starting web server", "main")
+
+	initializeFirebase()
 
 	sqlRepo := repository.NewSqlRepositoryImpl()
 	userRepo := repository.UserImpl{Sql: sqlRepo}
