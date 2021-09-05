af2f9a259c5fca42a4b7bed504a685bf3a7b2e7c
Shunei <info@shuneihayakawa.biz>
2021-09-05 15:47:00 +0900

    handled empty auth token case

--- infrastructure/web/middleware/auth.go
+++ infrastructure/web/middleware/auth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"harvest/core/exception"
 	"harvest/core/logger"
@@ -13,7 +14,13 @@ const aumComponent = "AuthMiddleware"
 
 func Auth(next http.Handler, auth repository.Auth) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token := request.GetAuthToken(r)
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+
+		token := strings.Fields(authHeader)[1]
 		if token == "" {
 			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 			return
