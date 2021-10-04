c4cd3680ed6bd9439199d789974f5b8360550961
Shunei <info@shuneihayakawa.biz>
2021-10-04 10:35:45 +0900

    removed custom claims

--- controller/user.go
+++ controller/user.go
@@ -1,20 +1,12 @@
 package controller
 
 import (
-	"strconv"
-
 	"harvest/domain/entity"
 	"harvest/domain/repository"
 )
 
 func CreateUser(user entity.User, userR repository.User, authR repository.Auth) error {
-	id, err := userR.Create(user)
-	if err != nil {
-		return err
-	}
-
-	claims := map[string]interface{}{"id": strconv.FormatInt(id, 10)}
-	err = authR.SetCustomClaim(user, claims)
+	_, err := userR.Create(user)
 	if err != nil {
 		return err
 	}
--- domain/repository/auth.go
+++ domain/repository/auth.go
@@ -1,8 +1,5 @@
 package repository
 
-import "harvest/domain/entity"
-
 type Auth interface {
 	VerifyToken(string) error
-	SetCustomClaim(entity.User, map[string]interface{}) error
 }
