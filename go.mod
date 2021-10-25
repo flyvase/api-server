da895423ca89f9bcd4d1b382cb36a4de1783807d
Shunei <info@shuneihayakawa.biz>
2021-10-25 19:24:10 +0900

    added mux from gorilla

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	firebase.google.com/go/v4 v4.6.0
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/rs/cors v1.8.0
 	github.com/stretchr/testify v1.6.1 // indirect
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	firebase "firebase.google.com/go/v4"
+	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	"google.golang.org/api/option"
 
@@ -52,17 +53,9 @@ func main() {
 	authRepo := &repositoryimpl.Auth{Client: auth}
 	spaceRepo := &repositoryimpl.Space{Sql: sql}
 
-	mux := http.NewServeMux()
-	mux.Handle("/user/", middleware.Demux(
-		&middleware.Group{
-			Post: handler.UserPost(authRepo, userRepo),
-		},
-	))
-	mux.Handle("/space/", middleware.Demux(
-		&middleware.Group{
-			Get: handler.SpaceGet(authRepo, spaceRepo),
-		},
-	))
+	mux := mux.NewRouter()
+	mux.Handle("/users/", middleware.Demux(&middleware.Group{Post: handler.UsersPost(authRepo, userRepo)}))
+	mux.Handle("/spaces/", middleware.Demux(&middleware.Group{Get: handler.SpacesGet(authRepo, spaceRepo)}))
 
 	c := cors.New(cors.Options{
 		AllowedOrigins: config.AllowedOrigin(),
