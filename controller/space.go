3112aa33310049345bc649a46d386419794e1bef
Shunei <info@shuneihayakawa.biz>
2021-10-26 18:16:20 +0900

    added FetchSpace method

--- controller/space.go
+++ controller/space.go
@@ -8,3 +8,7 @@ import (
 func ListSpaces(spaceR repository.Space) ([]entity.Space, error) {
 	return spaceR.List()
 }
+
+func FetchSpace(id uint32, spaceR repository.Space) (*entity.Space, error) {
+	return spaceR.Fetch(id)
+}
