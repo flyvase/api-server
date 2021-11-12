82cb43f570bf240623bbe630c8ebc6fa221702f9
shou-watanabe <watanabeshou1520@gmail.com>
2021-11-12 15:04:57 +0900

    HRVST-101 fix get space SQL

--- src/infrastructure/repository/space.go
+++ src/infrastructure/repository/space.go
@@ -11,7 +11,7 @@ type Space struct {
 
 func (sr *Space) List() ([]*entity.Space, error) {
 	rows, err := sr.Driver.Query(
-		"select * from spaces",
+		"select id, headline, access, number_of_visitors, main_customers_sex, min_main_customers_age, max_main_customers_age, price from spaces",
 	)
 
 	if err != nil {
@@ -24,7 +24,7 @@ func (sr *Space) List() ([]*entity.Space, error) {
 
 	for rows.Next() {
 		var se entity.Space
-		if err := rows.Scan(&se.Id, &se.Headline); err != nil {
+		if err := rows.Scan(&se.Id, &se.Headline, &se.Access, &se.NumberOfVisitors, &se.MainCustomersSex, &se.MinMainCustomersAge, &se.MaxMainCustomersAge, &se.Price); err != nil {
 			return nil, err
 		}
 
@@ -35,8 +35,8 @@ func (sr *Space) List() ([]*entity.Space, error) {
 }
 
 func (sr *Space) Fetch(id uint32) (*entity.Space, error) {
-	row, err := sr.Driver.QueryRow(
-		`select * from spaces where id = ?`,
+	spacesRow, err := sr.Driver.QueryRow(
+		`select id, headline, access, number_of_visitors, main_customers_sex, min_main_customers_age, max_main_customers_age, price, website_url from spaces where id = ?`,
 		id,
 	)
 
@@ -45,9 +45,51 @@ func (sr *Space) Fetch(id uint32) (*entity.Space, error) {
 	}
 
 	var se entity.Space
-	if err := row.Scan(&se.Id, &se.Headline); err != nil {
+	if err := spacesRow.Scan(&se.Id, &se.Headline, &se.Access, &se.NumberOfVisitors, &se.MainCustomersSex, &se.MinMainCustomersAge, &se.MaxMainCustomersAge, &se.Price, &se.WebsiteUrl); err != nil {
 		return nil, err
 	}
+
+	spaceImagesRows, err := sr.Driver.Query(
+		`select id, image_url from space_images where space_id = ?`,
+		id,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer spaceImagesRows.Close()
+
+	var spaceImages []*entity.SpaceImage
+	for spaceImagesRows.Next() {
+		var sie entity.SpaceImage
+		if err := spaceImagesRows.Scan(&sie.Id, &sie.ImageUrl); err != nil {
+			return nil, err
+		}
+		spaceImages = append(spaceImages, &sie)
+	}
+	se.Images = spaceImages
+
+	spaceDisplayersRows, err := sr.Driver.Query(
+		`select id, image_url, description from space_displayers where space_id = ?`,
+		id,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer spaceDisplayersRows.Close()
+
+	var spaceDisplayers []*entity.SpaceDisplayer
+	for spaceDisplayersRows.Next() {
+		var sde entity.SpaceDisplayer
+		if err := spaceDisplayersRows.Scan(&sde.Id, &sde.ImageUrl, &sde.Description); err != nil {
+			return nil, err
+		}
+		spaceDisplayers = append(spaceDisplayers, &sde)
+	}
+	se.Displayers = spaceDisplayers
 
 	return &se, nil
 }
