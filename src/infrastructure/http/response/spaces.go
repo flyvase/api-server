da1131310b0b1028ef5413f9bda0056352a7d435
shou-watanabe <watanabeshou1520@gmail.com>
2021-11-17 17:06:22 +0900

    HRVST-102 update reponse object

--- src/infrastructure/http/response/spaces.go
+++ src/infrastructure/http/response/spaces.go
@@ -22,18 +22,29 @@ type Spaces struct {
 }
 
 type SpaceDetail struct {
-	Id                  uint32                   `json:"id"`
-	Headline            string                   `json:"headline"`
-	Access              string                   `json:"access"`
-	NumberOfVisitors    uint32                   `json:"number_of_visitors"`
-	MainCustomersSex    string                   `json:"main_customers_sex"` // using string temporarily
-	MinMainCustomersAge uint8                    `json:"min_main_customers_age"`
-	MaxMainCustomersAge uint8                    `json:"max_main_customers_age"`
-	Price               uint32                   `json:"price"`
-	WebsiteUrl          string                   `json:"website_url"`
-	Coordinate          object.GeoPoint          `json:"coordinate"`
-	Images              []*entity.SpaceImage     `json:"images"`
-	Displayers          []*entity.SpaceDisplayer `json:"displayers"`
+	Id                  uint32           `json:"id"`
+	Headline            string           `json:"headline"`
+	Access              string           `json:"access"`
+	NumberOfVisitors    uint32           `json:"number_of_visitors"`
+	MainCustomersSex    string           `json:"main_customers_sex"` // using string temporarily
+	MinMainCustomersAge uint8            `json:"min_main_customers_age"`
+	MaxMainCustomersAge uint8            `json:"max_main_customers_age"`
+	Price               uint32           `json:"price"`
+	WebsiteUrl          string           `json:"website_url"`
+	Coordinate          object.GeoPoint  `json:"coordinate"`
+	Images              []SpaceImage     `json:"images"`
+	Displayers          []SpaceDisplayer `json:"displayers"`
+}
+
+type SpaceImage struct {
+	Id       uint32 `json:"id"`
+	ImageUrl string `json:"image_url"`
+}
+
+type SpaceDisplayer struct {
+	Id          uint32 `json:"id"`
+	ImageUrl    string `json:"image_url"`
+	Description string `json:"description"`
 }
 
 func EncodeSpaceEntities(entities []*entity.Space) ([]byte, error) {
@@ -54,6 +65,28 @@ func EncodeSpaceEntities(entities []*entity.Space) ([]byte, error) {
 }
 
 func EncodeSpaceEntity(entity *entity.Space) ([]byte, error) {
-	s := SpaceDetail{entity.Id, entity.Headline, entity.Access, entity.NumberOfVisitors, entity.MainCustomersSex, entity.MinMainCustomersAge, entity.MaxMainCustomersAge, entity.Price, entity.WebsiteUrl, entity.Coordinate, entity.Images, entity.Displayers}
+	spaceImages := EncodeSpaceImageEntities(entity.Images)
+	spaceDisplayer := EncodeSpaceDisplayerEntities(entity.Displayers)
+	s := SpaceDetail{entity.Id, entity.Headline, entity.Access, entity.NumberOfVisitors, entity.MainCustomersSex, entity.MinMainCustomersAge, entity.MaxMainCustomersAge, entity.Price, entity.WebsiteUrl, entity.Coordinate, spaceImages, spaceDisplayer}
 	return json.Marshal(s)
+}
+
+func EncodeSpaceImageEntities(entities []*entity.SpaceImage) []SpaceImage {
+	var spaceImages []SpaceImage
+	for _, sie := range entities {
+		si := SpaceImage{sie.Id, sie.ImageUrl}
+		spaceImages = append(spaceImages, si)
+	}
+
+	return spaceImages
+}
+
+func EncodeSpaceDisplayerEntities(entities []*entity.SpaceDisplayer) []SpaceDisplayer {
+	var SpaceDisplayers []SpaceDisplayer
+	for _, sde := range entities {
+		sd := SpaceDisplayer{sde.Id, sde.Description, sde.ImageUrl}
+		SpaceDisplayers = append(SpaceDisplayers, sd)
+	}
+
+	return SpaceDisplayers
 }
