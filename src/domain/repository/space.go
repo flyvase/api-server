21ce22ea5464a580d07cd4593ef5920745f1e934
Shunei <info@shuneihayakawa.biz>
2021-11-18 15:26:50 +0900

    added SpaceRepository interface

---
+++ src/domain/repository/space.go
@@ -0,0 +1,11 @@
+package repository
+
+import (
+	"harvest/src/domain/model"
+	"harvest/src/domain/value/space"
+)
+
+type Space interface {
+	Fetch(space.Id) (model.Space, error)
+	List() ([]*model.Space, error)
+}
