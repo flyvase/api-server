98d27e7462e78457731f392fc6d392026f725c07
Shunei <info@shuneihayakawa.biz>
2021-11-18 18:56:36 +0900

    modified Sex value object implementation

--- src/domain/value/core/sex.go
+++ src/domain/value/core/sex.go
@@ -1,5 +1,25 @@
 package core
 
-type Sex struct {
-	Value uint8
+type Sex string
+
+// Based on ISO5218
+const (
+	NotKnown      = Sex("0")
+	Male          = Sex("1")
+	Female        = Sex("2")
+	NotApplicable = Sex("9")
+)
+
+func SexFromString(s string) Sex {
+	switch s {
+	case "1":
+		return Male
+	case "2":
+		return Female
+	case "9":
+		return NotApplicable
+	// If invalid string is provided, it will return "not known"
+	default:
+		return NotKnown
+	}
 }
