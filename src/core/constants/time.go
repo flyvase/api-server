5e8f306915544e5400b966ece6a3a45eac304f7d
Shunei <info@shuneihayakawa.biz>
2021-11-18 18:57:14 +0900

    added constants for time

---
+++ src/core/constants/time.go
@@ -0,0 +1,13 @@
+package constants
+
+import "time"
+
+func WeekDuration() time.Duration {
+	d, _ := time.ParseDuration("168h")
+	return d
+}
+
+func DayDuration() time.Duration {
+	d, _ := time.ParseDuration("24h")
+	return d
+}
