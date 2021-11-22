135d2b7fe0a4a88237cebaab4f856c04545b97ff
Shunei <info@shuneihayakawa.biz>
2021-11-22 11:47:31 +0900

    added error handling to space listing endpoint

--- src/adapter/http/handler/spaces.go
+++ src/adapter/http/handler/spaces.go
@@ -2,15 +2,34 @@ package handler
 
 import (
 	"harvest/src/adapter/http/response"
+	"harvest/src/core/errors"
 	"harvest/src/domain/repository"
 	"net/http"
 )
 
 func SpacesGet(spaceRepository repository.Space) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		spaceModels, _ := spaceRepository.List()
+		spaceModels, err := spaceRepository.List()
+		if err != nil {
+			switch err.(type) {
+			case *errors.Unexpected:
+				{
+					http.Error(rw, "Unexpected error", http.StatusInternalServerError)
+					return
+				}
+			default:
+				{
+					http.Error(rw, "Data source unavailable", http.StatusInternalServerError)
+					return
+				}
+			}
+		}
 
-		json, _ := response.EncodeSpaceModels(spaceModels)
+		json, err := response.EncodeSpaceModels(spaceModels)
+		if err != nil {
+			http.Error(rw, "Failed to encode resources", http.StatusInternalServerError)
+			return
+		}
 
 		respond(rw, http.StatusOK, json)
 	})
