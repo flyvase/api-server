b5b57fde84a347edb85929ee46f58b0d24acafe9
Shunei <info@shuneihayakawa.biz>
2021-12-31 15:36:51 +0900

    removed stg env

--- src/config/http.go
+++ src/config/http.go
@@ -1,9 +1,7 @@
 package config
 
 func AllowedOrigin() []string {
-	if Environment == "stg" {
-		return []string{"https://retail-dot-flyvase-stg.an.r.appspot.com"}
-	} else if Environment == "prod" {
+	if Environment == "prod" {
 		return []string{"https://retail.flyvase.net"}
 	} else {
 		return []string{"http://localhost:3000"}
