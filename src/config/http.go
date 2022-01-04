f24cf616ccab83cff54ce01de38a7809f6010ec6
Shunei <info@shuneihayakawa.biz>
2022-01-04 12:26:56 +0900

    changed cors settings

--- src/config/http.go
+++ src/config/http.go
@@ -2,7 +2,7 @@ package config
 
 func AllowedOrigin() []string {
 	if Environment == "prod" {
-		return []string{"https://retail.flyvase.net"}
+		return []string{"https://seller.flyvase.net"}
 	} else {
 		return []string{"http://localhost:3000"}
 	}
