8243c58b09252bd23df46a9397d3e57e57f643d6
Shunei <info@shuneihayakawa.biz>
2022-01-04 09:07:43 +0900

    uncomment the auth middleware

--- src/infrastructure/http/middleware/auth.go
+++ src/infrastructure/http/middleware/auth.go
@@ -3,32 +3,33 @@ package middleware
 import (
 	"api-server/src/domain/repository"
 	"net/http"
+	"strings"
 )
 
 func Auth(authRepository repository.Auth, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		// authHeader := r.Header.Get("Authorization")
-		// if authHeader == "" {
-		// 	http.Error(rw, "Authorization header is required", http.StatusUnauthorized)
-		// 	return
-		// }
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(rw, "Authorization header is required", http.StatusUnauthorized)
+			return
+		}
 
-		// split := strings.Fields(authHeader)
-		// if len(split) != 2 {
-		// 	http.Error(rw, "Invalid Authorization header format", http.StatusUnauthorized)
-		// 	return
-		// }
+		split := strings.Fields(authHeader)
+		if len(split) != 2 {
+			http.Error(rw, "Invalid Authorization header format", http.StatusUnauthorized)
+			return
+		}
 
-		// token := split[1]
-		// if token == "" {
-		// 	http.Error(rw, "Authorization token is required", http.StatusUnauthorized)
-		// 	return
-		// }
+		token := split[1]
+		if token == "" {
+			http.Error(rw, "Authorization token is required", http.StatusUnauthorized)
+			return
+		}
 
-		// if err := authRepository.VerifyToken(token); err != nil {
-		// 	http.Error(rw, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-		// 	return
-		// }
+		if err := authRepository.VerifyToken(token); err != nil {
+			http.Error(rw, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
 
 		next.ServeHTTP(rw, r)
 	})
