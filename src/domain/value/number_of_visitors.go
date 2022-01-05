714ce8ce2457cfb4c96bfc97c893a82ef04ce9ec
Shunei <info@shuneihayakawa.biz>
2022-01-06 08:39:29 +0900

    added IsEmpty method to nullable value objects

--- src/domain/value/number_of_visitors.go
+++ src/domain/value/number_of_visitors.go
@@ -6,3 +6,11 @@ type NumberOfVisitors struct {
 	Visitors uint
 	Duration time.Duration
 }
+
+func (n *NumberOfVisitors) IsEmpty() bool {
+	if n.Visitors == 0 && n.Duration.Milliseconds() == 0 {
+		return true
+	}
+
+	return false
+}
